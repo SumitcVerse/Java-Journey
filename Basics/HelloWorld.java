882f63668a7b930e28809633e5df5b12402f049e
AlgoArchitect-Phoenix <sumitc9404@gmail.com>
2026-01-01 01:03:27 +0530

    Initial commit - Java basics

---
+++ Basics/HelloWorld.java
@@ -0,0 +1,11 @@
+  
+  // First Java program
+
+    public class HelloWorld{
+        public static void main(String[] args) {
+            System.out.println("Hello to my Java-Journey Verse!");
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+
+        }
+    }
